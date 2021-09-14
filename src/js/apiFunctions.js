3acba043dfda93291c9eebb1faed6c099e88cb05
Quan-p <quanp155@gmail.com>
2021-09-14 17:57:51 -0500

    Fixed typo

--- src/js/apiFunctions.js
+++ src/js/apiFunctions.js
@@ -12,7 +12,7 @@ function addWeather(data) {
 
 async function fetchWeather(location) {
     const key = '44417ab558331b507b15ba59e307962f';
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + key, {mode: 'cors'})
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + key, { mode: 'cors' })
         .then((response) => response.json())
         .then((data) => {
             addWeather(data);
