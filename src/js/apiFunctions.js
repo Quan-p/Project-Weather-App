f3302fed5ea5bc1bfdb7a8d9851aec421a7127e6
Quan-p <quanp155@gmail.com>
2021-09-14 14:57:38 -0500

    Processed json data and display to html

--- src/js/apiFunctions.js
+++ src/js/apiFunctions.js
@@ -1,11 +1,20 @@
 /* eslint-disable prefer-template */
 
+function addWeather(data) {
+    const celsius = Math.round(parseFloat(data.main.temp) - 273.15);
+    const fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+
+    document.getElementById('description').innerHTML = data.weather[0].description;
+    document.getElementById('temp').innerHTML = fahrenheit + '&deg';
+    document.getElementById('location').innerHTML = data.name;
+}
+
 function fetchWeather(location) {
     const key = '44417ab558331b507b15ba59e307962f';
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + key)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            addWeather(data);
         })
         .catch(() => {
             console.log('error');
