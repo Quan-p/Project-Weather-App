b0f10bdb305512f33e74ef012f0c204f6506d33e
Quan-p <quanp155@gmail.com>
2021-09-13 20:49:23 -0500

    Basic fetch function for weather

--- src/js/apiFunctions.js
+++ src/js/apiFunctions.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 function getData() {
     fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=44417ab558331b507b15ba59e307962f')
         .then((response) => {
@@ -8,4 +9,14 @@ function getData() {
         });
 }
 
-export default getData();
+function fetchWeather(location) {
+    const key = '{44417ab558331b507b15ba59e307962f}';
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + key)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch(() => {
+            console.log('error');
+        });
+}
