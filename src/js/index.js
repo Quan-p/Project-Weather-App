987ff1733c294e5a3f3e130d8f42ebb448e39c80
Quan-p <quanp155@gmail.com>
2021-09-15 19:09:43 -0500

    Add event listener to submit button to change location

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
 import fetchWeather from './apiFunctions';
 import getLocation from './search';
 
-function storeLocation() {
-    getLocation();
-    const location = inputVal;
-}
-
 window.onload = () => {
     fetchWeather('Austin');
 };
+
+function storeLocation() {
+    document.getElementById('locationInput').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const location = getLocation();
+        JSON.stringify(location);
+        fetchWeather('London');
+        console.log(location);
+    });
+}
+
+storeLocation();
